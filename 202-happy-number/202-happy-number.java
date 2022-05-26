6cef207d34962a783f3c0c6fa4cc8f4639d1611b
Ahmed Salah Heidar <70609687+ahmedheidar@users.noreply.github.com>
2022-05-26 03:19:23 +0200

    Time: 5 ms (10.11%), Space: 40.8 MB (68.88%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+        public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+    public boolean isHappy(int n){
+        Set<Integer> cycleMembers = new HashSet<>(Arrays.asList(4, 16, 37, 58, 89,                                                                               145,42,20));
+
+        while(n!=1 && !cycleMembers.contains(n)){
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}

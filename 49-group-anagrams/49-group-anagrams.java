d53ee96be8bbf5990731060c1d44b144ff7d3baa
Ahmed Salah Heidar <70609687+ahmedheidar@users.noreply.github.com>
2022-06-20 02:23:11 +0200

    Time: 13 ms (55.63%), Space: 55.4 MB (72.54%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,43 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> groups = new ArrayList<>();
-        for (String s1:strs) {
-            boolean foundGroup=false;
-            for (List<String> group:groups) {
-                String s2 = group.get(0);
-                if(validAnagram(s1,s2)){
-                    group.add(s1);
-                    foundGroup=true;
-                    break;
-                }
-            }
-            if(!foundGroup){
+                     List<List<String>> groups = new ArrayList<>();
+        HashMap<String,List<String>> strings = new HashMap<>();
+        for (String s:strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String wordSorted = String.valueOf(c);
+            if(strings.containsKey(wordSorted)){
+                strings.get(wordSorted).add(s);
+            }else{
                 List<String> newGroup = new ArrayList<>();
-                newGroup.add(s1);
-                groups.add(newGroup);
+                newGroup.add(s);
+                strings.put(wordSorted,newGroup);
             }
+        }
+        for (String key:strings.keySet()) {
+            groups.add(strings.get(key));
         }
         return groups;
-
-    }
-    
-    public static boolean validAnagram(String s, String t){
-        if(s.length()!=t.length()){
-            return false;
-        }
-        int[] arr = new int[26];
-        for (int i=0; i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-        }
-        for (int i=0; i<t.length();i++){
-            arr[t.charAt(i)-'a']--;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i]!=0){
-                return false;
-            }
-        }
-        return true;
 
     }
 }

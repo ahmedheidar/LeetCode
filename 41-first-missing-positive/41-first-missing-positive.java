e617be0e6519d1a8eaf3e8de065fb839188eb806
Ahmed Salah Heidar <70609687+ahmedheidar@users.noreply.github.com>
2022-08-07 01:02:22 +0200

    Time: 10 ms (24.90%), Space: 127.4 MB (51.44%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int min = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]>0 && nums[i]>min){
+                return min;
+            }
+            if(nums[i]==min){
+                min++;
+            }
+        }
+        return min;
+
+    }   
+}

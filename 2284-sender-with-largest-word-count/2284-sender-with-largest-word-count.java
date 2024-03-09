91d2f8ca48f4f1e2a767956bc60c0c6ca1a8f952
Ahmed Salah Heidar <70609687+ahmedheidar@users.noreply.github.com>
2024-03-09 21:20:58 +0200

    Time: 54 ms (47.78%), Space: 54.6 MB (56.30%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
@@ -1,23 +1,22 @@
 class Solution {
     public String largestWordCount(String[] messages, String[] senders) {
         HashMap<String, Integer> map = new HashMap<>();
-        
+        int max = -1;
+        String res = "";
         for(int i=0; i< messages.length; i++){
-            String[] words = messages[i].split(" ");
-            map.put(senders[i], map.getOrDefault(senders[i], 0)+ words.length);
+            int words = messages[i].split(" ").length;
+            map.put(senders[i], map.getOrDefault(senders[i], 0)+ words);
+            int c = map.get(senders[i]);
+            if(c>max){
+                res = senders[i];
+                max = c;
+            }
+            else if(c==max){
+                if(senders[i].compareTo(res)>0)
+                    res = senders[i];
+            }
         }
         
-        TreeSet<String> ts = new TreeSet<>((a, b) -> {
-            int countA = map.get(a);
-            int countB = map.get(b);
-            if(countA!=countB)
-                return Integer.compare(countB, countA);
-            return b.compareTo(a);
-        });
-
-        ts.addAll(map.keySet());
-
-        System.out.print(ts);
-        return ts.first();
+        return res;
     }
 }

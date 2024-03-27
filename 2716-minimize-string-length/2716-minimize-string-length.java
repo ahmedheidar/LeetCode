1760026d38557b314dfafbed6cf140f53ba764ce
Ahmed Salah Heidar <70609687+ahmedheidar@users.noreply.github.com>
2024-03-27 16:07:50 +0200

    Time: 8 ms (36.75%), Space: 45.4 MB (12.65%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.java
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -1,13 +1,9 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        StringBuilder sb = new StringBuilder();
         Set<Character> set = new HashSet();
-        sb.append(s.charAt(0));
         int i = 0;
         while(i<s.length()){
             set.add(s.charAt(i));
-            if(sb.charAt(sb.length()-1)!=s.charAt(i))
-                sb.append(s.charAt(i));
             i++;
         }
         
